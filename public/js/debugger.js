130a80971d700ea47bb7b4eb43db07cf1cd6bc46
sam <sam.w.mackinnon@gmail.com>
2017-04-05 22:08:00 -0700

    clean up

---
+++ public/js/debugger.js
@@ -0,0 +1,20 @@
+Debugger = function () {
+    $("body").append("<div id='DebugModal'><div id='DebugHeader'>Debug Console</div><div id='DebugBody'></div><div>")
+    db = {};
+    db.variables = [];
+    db.track = function (ref) {
+        console.log("this: " + JSON.stringify(ref));
+        db.variables.push(ref)
+        $("#DebugBody").append("<div id='name_" + ref + "'><strong>" + ref + ": </strong><span class='DebugVal' id='value" + (db.variables.length - 1) + "'></span></div>")
+    };
+    setInterval(function () {
+        for (var i = 0, ii = db.variables.length; i < ii; i++) {
+            var displayvar = eval(db.variables[i]);
+            if (typeof (displayvar) === "object") {
+                displayvar = JSON.stringify(displayvar);
+            }
+            $("#value" + i).text(displayvar);
+        }
+    }, 100);
+    return db;
+};
